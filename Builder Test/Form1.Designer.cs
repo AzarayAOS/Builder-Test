08c4800322ae5d22caa4aae26d0d0e21eecfb777
AOS <AOS@AOS-PC>
2016-06-03 23:43:45 +0300

    Добавьте файлы проекта.

---
+++ Builder Test/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Builder_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(684, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.открытьToolStripMenuItem1,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.открытьToolStripMenuItem.Text = "Создать";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // открытьToolStripMenuItem1
+            // 
+            this.открытьToolStripMenuItem1.Name = "открытьToolStripMenuItem1";
+            this.открытьToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.открытьToolStripMenuItem1.Text = "Открыть";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количесвто ответов на вопрс:";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Visible = false;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(328, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "OK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(531, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Варианты ответов";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Вопрос";
+            this.label3.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(374, 32);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(133, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Произвольный ответ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 35);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "OK";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Builder Test files|*.btf|Все файлы|*.*";
+            this.saveFileDialog1.Title = "Сохранение файла-опроса";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(700, 500);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Builder Test";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Builder Test/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace Builder_Test
+{
+    public partial class Form1 : Form
+    {
+        int nomber = 0; // количесвто вопросов
+        TextBox[] Tb;   // массив полей для ввода ответов
+        Label[] Lb;     // массив для подписей
+        int kolotv=0;     // количество элементов эдитов
+        string znach = "";// строка всех вопросов и ответов
+        int actotv;     // переменная для количества активных ответов (без произвольного)
+        string NewNabor(int ao) // тело функции формирования строки набора
+        {
+            String s = "";
+
+            // формируем команду начала набора
+            s += "<begin-" + nomber + "-" + kolotv + "-" + (checkBox1.Checked ? "t" : "f") + ">";
+
+
+            // ЗАНОСИМ В СТРОКУ ВОПРОС
+
+            // формируем команду вопроса
+            s += "<que>";
+
+            // заносим сам вопрос
+            s += textBox2.Text;
+
+            // формируем окончание команды вопроса
+            s += "<qend>";
+
+            // формируем команду ответов и сами ответы
+            for (int i = 0; i < ao; i++)
+            {
+                s += "<a-" + (i + 1) + ">";
+                s += Tb[i].Text;
+                s += "<aend-" + (i + 1) + ">";
+            }
+
+            // формируем команду окончания набора
+            s += "<bend-" + nomber + ">";
+            
+            // дабавляем команду переноса на новую строку
+            s += "\n";
+            return s;
+        }
+
+        // алгоритм дешифровки 
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Проверка аргументов.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Объявляем строку, используемую для хранения
+            // расшифрованного текста.
+            string plaintext = null;
+
+            // Создание RijndaelManaged объекта
+            // с указанным ключом и IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Создание декоратора для выполнения поток преобразования.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Создание потоков, используемых для дешифрования.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Чтение зашифрованных байтов из дешифрование потока
+                            // и помещение их в строку.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+        }
+
+
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Проверка аргументов
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            // Создание RijndaelManaged объекта
+            // с указанным ключом и IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Создание декоратора для выполнения поток преобразования.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Создание потоков, используемых для шифрования.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Запишите все данные в поток.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            // Возврат к зашифрованным байты из потока памяти.
+            return encrypted;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Height = 300;
+            this.Width = 300;
+            
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // устанавливаем размеры и положение всех элементов
+
+            // задаём размеры 
+            this.Height = 500;
+            this.Width = 700;
+           
+            
+            // задание расположения
+           // button1.Location = new Point(180, 390);
+           // button2.Location = new Point(420, 390);
+            label1.Location = new Point(170, 35);
+            textBox1.Location = new Point(333, 32);
+            button3.Location = new Point(400, 30);
+            checkBox1.Location = new Point(445, 33);
+
+            // делаем нужные объекты видимыми
+           // button1.Visible = true;
+           // button2.Visible = true;
+            label1.Visible = true;
+            textBox1.Visible = true;
+            button3.Visible = true;
+            textBox2.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            checkBox1.Visible = true;
+
+        }
+        // ограничиваем ввод в textBox1 только цифрами
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <= 47 || e.KeyChar >= 59) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+        // создание и редактирование вопроса
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // отчищаем поле вопроса
+            button3.Enabled = false;
+            button4.Enabled = true;
+            textBox2.Text = "";
+            //выводим в подпись формы количество вопросов
+            nomber++;
+            this.Text = "Builder Test    Количество вопросов: " + nomber.ToString();
+
+            // если эдиты ответов уже были созданы,
+            // удаляем их для создания новых
+            for (int i = 0; i < kolotv;i++)
+            {
+                Controls.Remove(Tb[i]);
+                Controls.Remove(Lb[i]);
+            }
+            
+            // делаем поле ввода вопроса активным
+            textBox2.Enabled = true;
+
+
+                kolotv = System.Convert.ToInt32(textBox1.Text);
+           
+// ------------------ создаем динамическое количество ответов
+            
+
+            // создаем массив количества вопросов и ответов
+            // если есть произвольный ответ, то создать эдитов и лейблов на один больше
+            Tb = new TextBox[kolotv];
+            Lb = new Label[kolotv];
+            for(int i=0, y=71+26;i<kolotv;i++,y+=26)
+            {
+                Tb[i] = new TextBox();
+                Tb[i].Location = new Point(159, y);
+                Tb[i].Name = "Tb" + i.ToString();
+                Tb[i].Size = new Size(513, 20);
+                //Tb[i].TabIndex = 15 + i+1;
+                Tb[i].TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+                Tb[i].Visible = true;
+                Controls.Add(Tb[i]);
+
+                Lb[i] = new Label();
+                Lb[i].Location = new Point(159 - 30, y);
+                Lb[i].Name = "Lb" + i.ToString();
+                Lb[i].Size = new Size(30, 15);
+                //Lb[i].TabIndex = 15 + i;
+                Lb[i].Text = (i + 1).ToString() + ")";
+                Lb[i].Visible = true;
+                Controls.Add(Lb[i]);
+            }
+             
+            // если есть произвольный ответ
+            if (checkBox1.Checked)
+            {
+                Tb[kolotv - 1].Enabled = false;
+                Tb[kolotv - 1].Text = "Это поле для произвольного ответа.";
+                Controls.Add(Tb[kolotv - 1]);
+            }
+
+            // формируем кнопку сохранения заданного набора
+            button4.Visible = true;
+            button4.Location = new Point(315,390);
+
+
+//============ ОБРАБАТЫВАЕМ ЗАПОЛНЕНИЕ ВСЕХ ПОЛЕЙ===============
+
+
+            // переменная для количества активных ответов (без произвольного)
+             actotv = checkBox1.Checked ? kolotv - 1 : kolotv;
+
+// Документация в коде. Тут я пропишу все внутрестроковые команды
+// по которым программа разделяет внутри строки, где какая информация
+// чтоб могла правильно интерпретировать заполненную внутри информацию
+// <begin-x-a-t> -- начало очередного набора, где x -- номер набора, 
+            //a -- количество активных ответов, 
+            //t(t\f)-- есть ли произвольный ответ
+        // <bend-x> -- конец набора, где x -- номер набора
+// <que> -- вопрос
+        //<qend> -- конец вопроса
+// <a-x> -- активный ответ, где x --  номер ответа
+        //<aend-x>
+//<t> -- произвольнгый ответ
+        //<tend>
+//<kol> -- количество вопросов
+        // <kend> 
+
+        }
+
+        // команда для запоминания введенный данных
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = true;
+            button4.Enabled = false;
+            // заносим введённые вопрос и ответы в общую строку
+            znach += NewNabor(actotv);
+
+            // делаем элементы не активными
+            textBox2.Enabled = false;
+            for (int i = 0; i < kolotv; i++)
+                Tb[i].Enabled = false;
+
+           
+
+
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // тут будем сохранять в файл информацию
+            // добавляем впереди количество вопросов
+            znach = "<kol>" + nomber.ToString() + "<kend>" + znach+"\n";
+
+            for (int i = 0; i < kolotv; i++)
+            {
+                Controls.Remove(Tb[i]);
+                Controls.Remove(Lb[i]);
+            }
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox2.Visible = false;
+            saveFileDialog1.FileName = "";
+            
+            if (saveFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+
+                string skey = "1212345678912443";
+                string siv = "1212345678912469";
+                //byte[] key = ASCIIEncoding.ASCII.GetBytes("1212345678912443");
+                //byte[] iV = ASCIIEncoding.ASCII.GetBytes("1212345678912469");
+
+                // пишим алгоритм шифрования 
+                try
+                {
+                    // Создание нового экземпляра RijndaelManaged
+                    // класс. Это принимает новый ключ и инициализации
+                    // вектор (IV).
+                    using (RijndaelManaged myRijndael = new RijndaelManaged())
+                    {
+
+
+                        //Этот алгоритм поддерживает ключи длиной 128, 192 и 256 бит или 16,24 и 32 байта(символа).
+                        byte[] key = ASCIIEncoding.ASCII.GetBytes(skey);
+                        byte[] iV = ASCIIEncoding.ASCII.GetBytes(siv);
+
+                        myRijndael.Key = key;
+                        myRijndael.IV = iV;
+
+                        // Зашифровать строку в массив байтов.
+                        byte[] encrypted = EncryptStringToBytes(znach, myRijndael.Key, myRijndael.IV);
+                        // открываем бинарный файл и записываем туда набор байтов
+                        File.WriteAllBytes(saveFileDialog1.FileName, encrypted);
+                        //textBox2.Text = DecryptStringFromBytes(File.ReadAllBytes(saveFileDialog1.FileName), myRijndael.Key, myRijndael.IV);
+                        ;
+                    }
+
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("Error: {0}", err.Message);
+                }
+
+            }
+        } 
+    }
+}
